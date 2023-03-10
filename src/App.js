d69fa1feb1aa8c6e86640433ba3a5407465f5cb5
Vinicius Predebon <kadiobaoff@gmail.com>
2023-03-10 00:50:15 -0300

    Versao com rotas das paginas

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import HomePage from "./pages/HomePage/HomePage"
 import SeatsPage from "./pages/SeatsPage/SeatsPage"
 import SessionsPage from "./pages/SessionsPage/SessionsPage"
 import SuccessPage from "./pages/SuccessPage/SuccessPage"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 export default function App() {
     return (
-        <>
-           <NavContainer>CINEFLEX</NavContainer>
+        <BrowserRouter>
+            <NavContainer>CINEFLEX</NavContainer>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/sessoes" element={<SessionsPage />} />
+                <Route path="/assentos" element={<SeatsPage />} />
+                <Route path="/sucesso" element={<SuccessPage />} />
+            </Routes>
 
-            {/* <HomePage /> */}
-            {/* <SeatsPage /> */}
-            <SessionsPage />
-            {/* <SuccessPage /> */}
-        </>
+
+        </BrowserRouter>
     )
 }
 
