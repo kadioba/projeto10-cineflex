e2b9b62bf7695752ac09315c32e5acd6352c907a
Vinicius Predebon <kadiobaoff@gmail.com>
2023-03-10 10:39:03 -0300

    Versao com pagina inicial mostrando os filmes da api corretamente

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,26 +1,29 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import ContainerFilme from "../../components/ContainerFilme";
+import React from "react";
 
 export default function HomePage() {
+
+    const [listaFilmes, setListaFilmes] = React.useState([]);
+
+    useEffect(() => {
+        const urlListaFilmes = "https://mock-api.driven.com.br/api/v8/cineflex/movies"
+        const requisicao = axios.get(urlListaFilmes);
+
+        requisicao.then(resposta => {
+            console.log(resposta);
+            setListaFilmes(resposta.data)
+        });
+    }, []);
+
     return (
         <PageContainer>
             Selecione o filme
 
             <ListContainer>
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
-
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
-
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
-
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
+                {listaFilmes.map(filme => <ContainerFilme key={filme.id} imagem={filme.posterURL} />)}
             </ListContainer>
 
         </PageContainer>
@@ -44,18 +47,4 @@ const ListContainer = styled.div`
     flex-wrap: wrap;
     flex-direction: row;
     padding: 10px;
-`
-const MovieContainer = styled.div`
-    width: 145px;
-    height: 210px;
-    box-shadow: 0px 2px 4px 2px #0000001A;
-    border-radius: 3px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 10px;
-    img {
-        width: 130px;
-        height: 190px;
-    }
 `
